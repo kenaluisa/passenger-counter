7d8294945d743ae31469bad70d83b0e64c3c2cfc
Kena Matzenbacher <matzenbacherkena@gmail.com>
2025-04-07 11:54:14 -0300

    Add passenger counter files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count");
+let saveEl = document.getElementById("save-el");
+let count = 0;
+// count = count + 1;
+
+// console.log(count);  control k ctrl c para comentar tudo
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+function save(){
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+    console.log(count);
+}
